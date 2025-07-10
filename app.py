903a6816cbb9d9b96a7eb1ff51bdb57ebac14a29
Xaradur <felixgloess@protonmail.com>
2025-07-10 14:57:50 +0200

    add route('/add) add template add.html add new styling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, request, render_template
 from storage import Storage
 
 app = Flask(__name__)
@@ -9,6 +9,24 @@ def index():
     blog_posts = data.list_posts()
     return render_template('index.html', posts=blog_posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+
+        try:
+            data.add({
+                "author": author,
+                "title": title,
+                "content": content
+            })
+            return redirect('/')
+        except ValueError as e:
+            return f"<p>Error: {e}</p>"
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
